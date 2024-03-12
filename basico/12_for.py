9f92afe69b033ffda818bc8389caf68ddb9549e7
GabrielNP96 <gabrielnp1996@gmail.com>
2024-03-12 04:40:32 -0300

    conceitos de for e while

---
+++ basico/12_for.py
@@ -0,0 +1,4 @@
+#for serve para iterar
+lista = [1,2,3,4,5,6,7,8,9,10]
+for numbers in lista:
+    print(numbers)
---
+++ basico/13_while.py
@@ -0,0 +1,6 @@
+#While é um loop de repetição
+#criando um contador de 0 até 10
+counter = 0
+while counter < 11:
+    print(counter)
+    counter += 1
